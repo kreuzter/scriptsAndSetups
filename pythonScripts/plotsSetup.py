f578c868d637cf8ff49ba2cfe2080713276a1c5b
kreuzter <kreuzova.t@gmail.com>
2026-01-13 21:19:09 +0100

    unified plots in python

---
+++ pythonScripts/plotsSetup.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+plt.rcParams.update({
+  "text.usetex": True,
+  "font.family": "serif",
+  "font.serif" : "Computer Modern",
+  "font.size"  : 14,
+  'axes.spines.right' : False,
+  'axes.spines.top'   : False,
+  #'axes.spines.left'  : False,
+  #'axes.spines.bottom': False,
+  'legend.fancybox' : False,
+  'legend.edgecolor' : 'k'
+})
+
+
