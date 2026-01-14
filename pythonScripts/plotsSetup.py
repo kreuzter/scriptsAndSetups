c1a6e50d996fb71f43fb3d585f78c06b856e7641
Terezie Kreuzova <kreuzova.t@gmail.com>
2026-01-14 10:40:33 +0100

    update pythonScripts - plotsDesign

--- pythonScripts/plotsSetup.py
+++ pythonScripts/plotsSetup.py
@@ -1,16 +1,26 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import cycler
 
-plt.rcParams.update({
-  "text.usetex": True,
-  "font.family": "serif",
-  "font.serif" : "Computer Modern",
-  "font.size"  : 14,
-  'axes.spines.right' : False,
-  'axes.spines.top'   : False,
-  #'axes.spines.left'  : False,
-  #'axes.spines.bottom': False,
-  'legend.fancybox' : False,
-  'legend.edgecolor' : 'k'
-})
+def update_rcParams(nCycler = 10, params = {}):
+
+  paramsDefault = {
+    "text.usetex": True,
+    "font.family": "serif",
+    "font.serif" : "Computer Modern",
+    "font.size"  : 14,
+    'axes.spines.right' : False,
+    'axes.spines.top'   : False,
+    #'axes.spines.left'  : False,
+    #'axes.spines.bottom': False,
+    'legend.fancybox' : False,
+    'legend.edgecolor' : 'k'
+  }
+  paramsDefault.update(params)
+  paramsDefault.update({
+    'axes.prop_cycle' : cycler.cycler('color', plt.cm.jet(np.linspace(0.0, 0.91,nCycler))),
+  })
+
+  plt.rcParams.update( paramsDefault )
 
 
